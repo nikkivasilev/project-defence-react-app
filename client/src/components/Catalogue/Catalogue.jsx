3b3c0a35bebef8016f2ae80e9e76bca840318032
nikkivasilev <n.vasilev105@gmail.com>
2023-11-16 18:52:14 +0200

    fixed condition

--- client/src/components/Catalogue/Catalogue.jsx
+++ client/src/components/Catalogue/Catalogue.jsx
@@ -3,17 +3,13 @@ import CatalogueItem from "../CatalogueItem/CatalogueItem"
 export default function Catalogue(products) {
     return (
         <section id="catalogPage">
-         {products.products ? ( 
+         {products.products.length > 0 ? ( 
             <>  
-
                 {products.products.map(x => <CatalogueItem key={x._id} {...x}/>)}
             </>
-        ) :  
-            (
-                <>
-            <p>No Products in Catalog!</p>
-            </>
-            ) } 
+        ) : <> <p>No Products in Catalog!</p>   </>
+    
+        } 
         </section>
     )
 }
