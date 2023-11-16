b7f79c29addde3501322af0d3e9ff3be390fc18b
nikkivasilev <n.vasilev105@gmail.com>
2023-11-16 19:01:44 +0200

    made the items dynamic

--- client/src/components/CatalogueItem/CatalogueItem.jsx
+++ client/src/components/CatalogueItem/CatalogueItem.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
-
-export default function CatalogueItem() {
+export default function CatalogueItem(props) {
+    const productUrl = `/product/details/${props._id}`
     return (
         <div className="card-box">
                     <img className="ProductImg" src="/images/no-image-found.jpg" alt="product Image"/>
                     <div>
                         <div className="text-center ">
-                            <p className="name">Name: Ime Na Product</p>
-                            <p className="price">Price: 12 BGN</p>
+                            <p className="name">Name: {props.name}</p>
+                            <p className="price">Price: {props.price} BGN</p>
                         </div>
                         <div className="btn-group">
-                            <Link to="/product/details/1">Details</Link>
+                            <Link to={productUrl}>Details</Link>
                         </div>
                     </div>
         </div>
