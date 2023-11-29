05794c38b8f00c41557e179b1c5319acc5fccdbe
nikkivasilev <n.vasilev105@gmail.com>
2023-11-29 23:10:04 +0200

    refactored the user details page

--- client/src/components/UserDetails/UserDetails.jsx
+++ client/src/components/UserDetails/UserDetails.jsx
@@ -1,24 +1,23 @@
+import { useContext } from "react";
+import AuthContext from "../../contexts/authContext";
+
 export default function UserDetails() {
+    const { email, username} = useContext(AuthContext);
+
     return (
         <div className="wrapper">
-        {/* {% if user.profile_picture %} */}
-            {/* <div className="profilePage">
-                <img src="{{ user.profile_picture.url }}"
-                     alt="Profile Image"/>
-            </div> */}
-        {/* {% else %} */}
-            <div className="profilePage">
-                <img src="/images/blank-profile-picture-973460_1280.jpeg"
-                     alt="Profile Image"/>
-            </div>
-        {/* {% endif %} */}
         <div className="profileText">
-            <h1>Username: goshoo</h1>
-            <h1>Email: gosho@gosho.com</h1>
+            {username !== email && 
+            <h1>Username: {username}</h1>
+            }
+            {username === email &&
+            <h1>Username: No username</h1>
+            }
+            <h1>Email: {email}</h1>
         </div>
         <div className="actionBtn">
             <a href="{% url 'edit user' pk=request.user.pk %}" className="remove">Edit</a>
-            <a href="{% url 'liked products'%}" className="remove">My products</a>
+            {/* <a href="{% url 'liked products'%}" className="remove">My products</a> */}
         </div>
         <div className="actionBtnDelete">
             <a href="{% url 'delete user' pk=request.user.pk %}">Delete</a>
